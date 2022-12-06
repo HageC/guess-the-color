e9c0df9524b47fc6cc3a40a27d89d51f4d687ca1
Christopher Hage <hage.chris@yahoo.com>
2022-12-05 21:25:32 -0400

    app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState([]);
+  const [color, setColor] = useState("");
+  const [message, setMessage] = useState("");
+  const [score, setScore] = useState(0);
+  useEffect(() => {
+    setColors([generateColor(), generateColor(), generateColor()]);
+  }, []);
+
+  useEffect(() => {
+    const index = Math.floor(Math.random() * 3);
+    setColor(colors[index]);
+  }, [colors]);
+
+  const generateColor = () => {
+    return (
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")
+    );
+  };
+
+  const checkAnswer = (e) => {
+    if (e.target.innerHTML === color) {
+      setMessage("That is correct!");
+      setScore((prev) => prev + 1);
+    } else {
+      setMessage(`That is incorrect, the right answer was ${color}.`);
+      setScore(0);
+    }
+
+    setColors([generateColor(), generateColor(), generateColor()]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <h1 className="header">Guess the Color</h1>
+        <div className="color" style={{ backgroundColor: color }}></div>
+
+        <div className="options">
+          {colors.map((color, index) => {
+            return (
+              <div key={index} className="option" onClick={checkAnswer}>
+                {color}
+              </div>
+            );
+          })}
+        </div>
+        <p className="message">{message}</p>
+        <p className="score">Score: {score}</p>
+      </main>
     </div>
   );
 }
